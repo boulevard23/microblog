1f8c1d6032bf32ebc275ab009167f3acc9d33a2f
Chao Zhang <zhang.chao.eric@gmail.com>
2013-10-03 15:37:34 -0400

    Implemented the pagination

--- app/views.py
+++ app/views.py
@@ -4,6 +4,7 @@ from app import app, db, lm, oid
 from forms import LoginForm, EditForm, PostForm
 from models import User, ROLE_USER, ROLE_ADMIN, Post
 from datetime import datetime
+from config import POSTS_PER_PAGE
 
 @app.before_request
 def before_request():
@@ -15,8 +16,9 @@ def before_request():
 
 @app.route('/', methods = ['GET', 'POST'])
 @app.route('/index', methods = ['GET', 'POST'])
+@app.route('/index/<int:page>', methods = ['GET', 'POST'])
 @login_required
-def index():
+def index(page = 1):
   form = PostForm()
   if form.validate_on_submit():
     post = Post(body = form.post.data, timestamp = datetime.utcnow(), author = g.user)
@@ -24,7 +26,7 @@ def index():
     db.session.commit()
     flash('Your post is now live!')
     return redirect(url_for('index'))
-  posts = g.user.followed_posts().all()
+  posts = g.user.followed_posts().paginate(page, POSTS_PER_PAGE, False)
 
   return render_template('index.html',
       title = 'Home',
@@ -74,16 +76,14 @@ def logout():
   return redirect(url_for('index'))
 
 @app.route('/user/<nickname>')
+@app.route('/user/<nickname>/<int:page>')
 @login_required
-def user(nickname):
+def user(nickname, page = 1):
   user = User.query.filter_by(nickname = nickname).first()
   if user == None:
     flash('User ' + nickname + ' not found.')
     return redirect(url_for('index'))
-  posts = [
-      { 'author': user, 'body': 'Test post #1' },
-      { 'author': user, 'body': 'Test post #2' }
-    ]
+  posts = user.posts.paginate(page, POSTS_PER_PAGE, False)
 
   return render_template('user.html',
       user = user,
