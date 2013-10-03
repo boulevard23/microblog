31b14c2941919c4828daeed1bcbce819fa823cfa
Chao Zhang <zhang.chao.eric@gmail.com>
2013-10-02 21:24:25 -0400

    Added follow and unfollow logic, and passed the unit test

--- app/models.py
+++ app/models.py
@@ -4,14 +4,38 @@ from hashlib import md5
 ROLE_USER = 0
 ROLE_ADMIN = 1
 
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
 class User(db.Model):
   id = db.Column(db.Integer, primary_key = True)
-  nickname = db.Column(db.String(64), index = True, unique = True)
+  nickname = db.Column(db.String(64), unique = True)
   email = db.Column(db.String(120), index = True, unique = True)
   role = db.Column(db.SmallInteger, default = ROLE_USER)
   posts = db.relationship('Post', backref = 'author', lazy = 'dynamic')
   about_me = db.Column(db.String(140))
   last_seen = db.Column(db.DateTime)
+  followed = db.relationship('User',
+      secondary = followers,
+      primaryjoin = (followers.c.follower_id == id),
+      secondaryjoin = (followers.c.followed_id == id),
+      backref = db.backref('followers', lazy = 'dynamic'),
+      lazy = 'dynamic')
+
+  def follow(self, user):
+    if not self.is_following(user):
+      self.followed.append(user)
+      return self
+
+  def unfollow(self, user):
+    if self.is_following(user):
+      self.followed.remove(user)
+      return self
+
+  def is_following(self, user):
+    return self.followed.filter(followers.c.followed_id == user.id).count() > 0
 
   @staticmethod
   def make_unique_nickname(nickname):
@@ -51,3 +75,4 @@ class Post(db.Model):
 
   def __repr__(self):
     return '<Post %r>' % (self.body)
+
